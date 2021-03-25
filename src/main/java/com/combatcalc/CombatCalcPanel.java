798c41f577b368eb8610f5c2f9a33cf42cf9bd51
Brian Westheimer <brwestheimer@gmail.com>
2021-03-25 12:44:30 -0400

    Add levels needed to level up functionality.

--- src/main/java/com/combatcalc/CombatCalcPanel.java
+++ src/main/java/com/combatcalc/CombatCalcPanel.java
@@ -19,11 +19,18 @@ public class CombatCalcPanel extends PluginPanel {
     private final double COMBAT_MULTIPLIER = .325;
     private final double RANGE_MAGE_MULTIPLIER = 1.5;
     private final double BASE_MULTIPLIER = .25;
-    private int baseCombatLevel;
-    private int meleeCombatLevel;
-    private int rangedCombatLevel;
-    private int magicCombatLevel;
-    private int combatLevel;
+    private double baseCombatLevel;
+    private double meleeCombatLevel;
+    private double rangedCombatLevel;
+    private double magicCombatLevel;
+    private double combatLevel;
+    private double prayerNeeded;
+    private double defenceNeeded;
+    private double hitpointsNeeded;
+    private double attackNeeded;
+    private double strengthNeeded;
+    private double magicNeeded;
+    private double rangeNeeded;
 
     CombatCalcPanel(Client client){
         this.client = client;
@@ -79,35 +86,99 @@ public class CombatCalcPanel extends PluginPanel {
         }else{
             combatLevel = magicCombatLevel +baseCombatLevel;
         }
+
+        calculatePrayerNeeded();
+        calculateDefenceNeeded();
+        calculateHitpointsNeeded();
+        calculateAttackNeeded();
+        calculateStrengthNeeded();
+        calculateRangeNeeded();
+        calculateMagicNeeded();
+
         log.info(String.valueOf(combatLevel));
     }
 
     private void calculateBaseCombat(){
-        this.baseCombatLevel = (int)(BASE_MULTIPLIER * ((Integer.parseInt(combatInput.getPrayerField().getText()) / 2) +
+        this.baseCombatLevel = (BASE_MULTIPLIER * ((Integer.parseInt(combatInput.getPrayerField().getText()) / 2) +
                 Integer.parseInt(combatInput.getDefenceField().getText()) +
                 Integer.parseInt(combatInput.getHitpointsField().getText())));
         log.info("Your base combat level is " + this.baseCombatLevel);
     }
 
     private void calculateMeleeCombat(){
-        this.meleeCombatLevel = (int)(COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getAttackField().getText()) +
+        this.meleeCombatLevel = (COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getAttackField().getText()) +
                 Integer.parseInt(combatInput.getStrengthField().getText())));
         log.info("Your melee combat level is " + this.meleeCombatLevel);
     }
 
     private void calculateRangedCombat(){
-        this.rangedCombatLevel = (int)(COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getRangedField().getText()) *
+        this.rangedCombatLevel = (COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getRangedField().getText()) *
                 RANGE_MAGE_MULTIPLIER));
         log.info("Your ranged combat level is " + this.rangedCombatLevel);
     }
 
     private void calculateMagicCombat(){
-        this.magicCombatLevel = (int)(COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getMagicField().getText()) *
+        this.magicCombatLevel = (COMBAT_MULTIPLIER * (Integer.parseInt(combatInput.getMagicField().getText()) *
                 RANGE_MAGE_MULTIPLIER));
         log.info("Your magic combat level is " + this.magicCombatLevel);
     }
 
+    private void calculatePrayerNeeded(){
+        double nextBaseLevel = ((int)this.combatLevel + 1) - this.meleeCombatLevel;
+        double nextPrayerLevel = (((nextBaseLevel * 4) - Integer.parseInt(combatInput.getDefenceField().getText())
+                - Integer.parseInt(combatInput.getHitpointsField().getText())) * 2);
+        this.prayerNeeded = nextPrayerLevel - Integer.parseInt(combatInput.getPrayerField().getText());
+
+        log.info("To level up with prayer you need " + (int)this.prayerNeeded + " levels!");
+    }
+
+    private void calculateDefenceNeeded(){
+        double nextBaseLevel = ((int)this.combatLevel + 1) - this.meleeCombatLevel;
+        double nextDefLevel = ((nextBaseLevel * 4) - Integer.parseInt(combatInput.getHitpointsField().getText())
+                - (Integer.parseInt(combatInput.getPrayerField().getText()) / 2));
+        this.defenceNeeded = nextDefLevel - Integer.parseInt(combatInput.getDefenceField().getText());
+        log.info("To level up with defence you need " + (int)this.defenceNeeded + " levels!");
+    }
+
+    private void calculateHitpointsNeeded(){
+        double nextBaseLevel = ((int)this.combatLevel + 1) - this.meleeCombatLevel;
+        double nextHPLevel = ((nextBaseLevel * 4) - Integer.parseInt(combatInput.getDefenceField().getText())
+                - (Integer.parseInt(combatInput.getPrayerField().getText()) / 2));
+        this.hitpointsNeeded = nextHPLevel - Integer.parseInt(combatInput.getHitpointsField().getText());
+        log.info("To level up with hitpoints you need " + (int)this.hitpointsNeeded + " levels!");
+    }
+
+    private void calculateAttackNeeded(){
+        double nextMeleeLevel = ((int) this.combatLevel +1) - this.baseCombatLevel;
+        double nextAttackLevel = ((nextMeleeLevel / this.COMBAT_MULTIPLIER)
+                - Integer.parseInt(combatInput.getStrengthField().getText()));
+        this.attackNeeded = nextAttackLevel - Integer.parseInt(combatInput.getAttackField().getText());
+        log.info("To level up with attack you need " + (int)this.attackNeeded + " levels!");
+    }
+
+    private void calculateStrengthNeeded(){
+        double nextMeleeLevel = ((int) this.combatLevel +1) - this.baseCombatLevel;
+        double nextStrLevel = ((nextMeleeLevel / this.COMBAT_MULTIPLIER)
+                - Integer.parseInt(combatInput.getAttackField().getText()));
+        this.strengthNeeded = nextStrLevel - Integer.parseInt(combatInput.getStrengthField().getText());
+        log.info("To level up with strength you need " + (int)this.strengthNeeded + " levels!");
+    }
+
+    private void calculateMagicNeeded(){
+        double nextMagicCBLevel = ((int) this.combatLevel + 1) - this.baseCombatLevel;
+        double nextMagicLevel = ((nextMagicCBLevel / COMBAT_MULTIPLIER) / RANGE_MAGE_MULTIPLIER);
+        this.magicNeeded = nextMagicLevel - Integer.parseInt(combatInput.getMagicField().getText());
+        log.info("To level up with magic you need " + (int)this.magicNeeded + " levels!");
+    }
+
+    private void calculateRangeNeeded(){
+        double nextMagicCBLevel = ((int) this.combatLevel + 1) - this.baseCombatLevel;
+        double nextMagicLevel = ((nextMagicCBLevel / COMBAT_MULTIPLIER) / RANGE_MAGE_MULTIPLIER);
+        this.rangeNeeded = nextMagicLevel - Integer.parseInt(combatInput.getRangedField().getText());
+        log.info("To level up with range you need " + (int)this.rangeNeeded + " levels!");
+    }
+
     public void updateOutput(){
-        this.combatOutput.setCombatValue(combatLevel);
+        this.combatOutput.setCombatValue((int)combatLevel);
     }
 }
